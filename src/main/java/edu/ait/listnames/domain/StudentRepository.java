58ff0c61e75b7026b27b53e151b0f9be2696763c
李国平 <lgppppppp@163.com>
2021-03-31 00:57:43 +0800

    学生页面domain

--- src/main/java/edu/ait/listnames/domain/StudentRepository.java
+++ src/main/java/edu/ait/listnames/domain/StudentRepository.java
@@ -2,8 +2,6 @@ package edu.ait.listnames.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface StudentRepository extends JpaRepository<Student, String>, JpaSpecificationExecutor<Student> {
 
